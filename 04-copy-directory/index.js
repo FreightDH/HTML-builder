968f342c70d50b0224e74736237d6dee63f2744b
FreightDH <maxim.zolotkov@yandex.ru>
2023-05-07 19:44:10 +0300

    feat: add solution of 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) console.error(err.message);
+});
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  if (err) console.error(err.message);
+
+  files.forEach((file) => {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+      if (err) console.error(err.message);
+    });
+  });
+});
