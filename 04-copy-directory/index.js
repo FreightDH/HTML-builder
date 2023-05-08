a1d6620f086a6b0310b8693766f0806f799fc034
FreightDH <maxim.zolotkov@yandex.ru>
2023-05-08 16:37:33 +0300

    feat: add folder update 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,6 +8,18 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
 fs.readdir(path.join(__dirname, 'files'), (err, files) => {
   if (err) console.error(err.message);
 
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, copyFiles) => {
+    if (err) console.error(err.message);
+
+    copyFiles.forEach(element => {
+      if (!files.includes(element)) {
+        fs.rm(path.join(__dirname, 'files-copy', element), (err) => {
+          if (err) console.error(err.message);
+        });
+      }
+    });
+  });
+
   files.forEach((file) => {
     fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
       if (err) console.error(err.message);
