c7f280a47a57828bf05c3c248873658c3a937f41
FreightDH <maxim.zolotkov@yandex.ru>
2023-05-08 13:00:59 +0300

    refactor: path to files in 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFile(path.join(__dirname, 'test-files', 'bundle.css'), '', (err) => {
+fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '', (err) => {
   if (err) console.error(err.message);
 });
 
-fs.readdir(path.join(__dirname, 'test-files','styles'), (err, files) => {
+fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
   if (err) console.error(err.message);
 
   files.forEach((file) => {
-    fs.stat(path.join(__dirname, 'test-files','styles', file), (err, stats) => {
+    fs.stat(path.join(__dirname, 'styles', file), (err, stats) => {
       if (err) console.error(err.message);
 
       if (!stats.isDirectory() && path.extname(file) === '.css') {
-        fs.readFile(path.join(__dirname, 'test-files','styles', file), (err, data) => {
+        fs.readFile(path.join(__dirname, 'styles', file), (err, data) => {
           if (err) console.error(err.message);
 
-          fs.appendFile(path.join(__dirname, 'test-files', 'bundle.css'), data, (err) => {
+          fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data, (err) => {
             if (err) console.error(err.message);
           });
         })
